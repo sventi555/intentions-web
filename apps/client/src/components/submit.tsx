a05b28ed66b7d1c820911a08310bf70d296c0efd
Stephen Edwards <34760298+sventi555@users.noreply.github.com>
2025-10-26 23:44:19 -0400

    add disabled prop to submit input

--- apps/client/src/components/submit.tsx
+++ apps/client/src/components/submit.tsx
@@ -9,6 +9,7 @@ export const Submit: React.FC<SubmitProps> = (props) => {
   return (
     <input
       type="submit"
+      disabled={props.disabled}
       value={props.label}
       className={getButtonStyle({ type: 'primary', disabled: props.disabled })}
     />
